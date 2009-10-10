f14c1b85db316645709d59e434131edf677a8879
hn3000 <harald@parkverbot.org>
2009-10-10 22:03:32 +0200

    some example projects ...

---
+++ HelloWorld++/HelloWorld++.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HelloWorld++.cpp
+// Author      : Eclipse wrote this code as a C++ example :-S
+// Version     :
+// Copyright   : yeah right
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!");
+	return EXIT_SUCCESS;
+}
---
+++ HelloWorldJ/src/com/github/hn3000/hello/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.github.hn3000.hello;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
